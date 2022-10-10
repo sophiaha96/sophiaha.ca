1508bf69ea9a1311f351cd98db19e2c3442dfdc3
Sophia Ha <sophiaha96@gmail.com>
2022-10-10 03:33:05 -0400

    fix: move non-prerendered pages to components

--- lib/db.ts
+++ lib/db.ts
@@ -5,7 +5,6 @@ import { isRedisEnabled, redisUrl, redisNamespace } from './config';
 
 let db: Keyv;
 if (isRedisEnabled) {
-  console.log('\n\nREDIS URL:', redisUrl, '\n');
   const keyvRedis = new KeyvRedis(redisUrl);
   db = new Keyv({ store: keyvRedis, namespace: redisNamespace || undefined });
 } else {
